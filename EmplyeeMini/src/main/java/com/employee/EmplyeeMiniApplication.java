1c8050ec50a204ae62db1aec9d8a345e3805db9a
shashank <shashank.cse2015@gmail.com>
2022-03-28 18:12:28 +0530

    Initail commit

---
+++ EmplyeeMini/src/main/java/com/employee/EmplyeeMiniApplication.java
@@ -0,0 +1,14 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyeeMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplyeeMiniApplication.class, args);
+		System.out.println("Employee Mini Application Started");
+	}
+
+}
